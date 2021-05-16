2efaea7c51e7ad5ae2198dc95ea8346505f07457
jinnny-github <71935795+jinnny-github@users.noreply.github.com>
2021-05-16 20:53:17 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,22 +2,42 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
+
 mongoose
-  .connect(
-    "mongodb+srv://jinnny:jinnny1234@boilerplate.xlgz4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! NodeMon Test");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입 시 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
